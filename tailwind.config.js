2284bd0b876688d0ebf51ee1fecb0a2da3c5e5e1
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2023-03-23 20:55:27 -0600

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   content: ['./*.html'],
   plugins: [],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+    },
+  },
 };
